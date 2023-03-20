2881754117c23116377753b9be62c15ef9a9b4b4
Marko2002Grujicic <m.grujicic2002@gmail.com>
2023-03-20 08:39:55 +0100

    fixed small isuess

--- src/App.js
+++ src/App.js
@@ -38,6 +38,7 @@ function App() {
         <Route path="/" element={<MainContent shows={shows} searchQuery={searchQuery} setSearchQuery={setSearchQuery} />}/>
         <Route path="/shows/:id" element={<SingleShowPage layout={layout} setLayout={setLayout} isLoading={isLoading} setIsLoading={setIsLoading}/>}/>
         <Route path="/about" element={<About/>}/>
+        <Route path='*' element={<MainContent shows={shows} searchQuery={searchQuery} setSearchQuery={setSearchQuery} />}/>
       </Routes>
       <Footer/>
     </div>
